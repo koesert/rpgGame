e316934893b3d0acebbc4afe4c5380613b46be32
koesert <janwillemvanbochove@gmail.com>
2024-02-12 15:09:21 +0100

    added functionality to ask users first and lastname and add them together

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void User()
+    {
+        Console.WriteLine("What is your first name?");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your full name is: {firstName} {lastName}");
+    }
+}
